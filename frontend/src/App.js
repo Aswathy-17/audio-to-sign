6b88d88779b62e917b110e4a7a4bd747557093ec
EVITTATELSON <evittatelson@gmail.com>
2024-11-14 23:20:40 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to Our Website</h1>
+      <p>
+        Discover our features and services, learn more about us, and explore our offerings to get the best experience.
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default App;
